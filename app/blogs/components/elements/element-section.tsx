666c6520a447242ffc916087da9d2d36a12c9092
unknown <jeetshahajwa@gmail.com>
2024-05-22 21:45:58 +0530

    ADded Icons

--- app/blogs/components/elements/element-section.tsx
+++ app/blogs/components/elements/element-section.tsx
@@ -1,5 +1,6 @@
 import Accordion from "@/app/components/accordion/accordion";
 import React, { useState } from "react";
+import { Icon } from "@iconify/react";
 
 const ElementSection = () => {
   const [current, setCurrent] = useState(0);
@@ -11,17 +12,21 @@ const ElementSection = () => {
         onClick={() => setCurrent(current === 0 ? -1 : 0)}
         title="input box"
       >
-        <input
-          placeholder="Label"
-          className="bg-primary dark:bg-dark-secondary outline-none py-2 px-4 rounded"
-          disabled
-        />
+        <div className="bg-primary dark:bg-dark-secondary outline-none py-2 px-4 rounded flex justify-between items-center">
+          <p>Label</p>
+          <div className="flex gap-2 items-center">
+            <Icon fontSize={"24px"} icon={"gg:add"} />
+            <Icon fontSize={"24px"} icon={"material-symbols:info-outline"} />
+          </div>
+        </div>
         <div className="py-3" />
-        <textarea
-          placeholder="Paragraph"
-          className=" bg-primary dark:bg-dark-secondary outline-none py-2 px-4 rounded h-20 w-full"
-          disabled
-        ></textarea>
+        <div className=" bg-primary dark:bg-dark-secondary outline-none py-2 px-4 rounded h-20 w-full  flex justify-between items-start">
+          <p>Paragrah</p>
+          <div className="flex gap-2 items-center">
+            <Icon fontSize={"24px"} icon={"gg:add"} />
+            <Icon fontSize={"24px"} icon={"material-symbols:info-outline"} />
+          </div>
+        </div>
         <div className="py-3" />
 
         <picture>
