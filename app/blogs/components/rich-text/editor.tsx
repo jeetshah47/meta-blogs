f01a02036a409dad6618f095b22b3f190cf9a6a4
unknown <jeetshahajwa@gmail.com>
2024-05-27 11:08:41 +0530

    ck editor

--- app/blogs/components/rich-text/editor.tsx
+++ app/blogs/components/rich-text/editor.tsx
@@ -1,19 +1,31 @@
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import { useRef } from 'react';
 
 const  Editor = () => {
+  const ref = useRef(null);
+  
   return (
     <div className='App'>
-      <h2>CKEditor 5 React App</h2>
       <CKEditor
         editor={ ClassicEditor }
-        data="<p>Hello from CKEditor 5!</p>"
+        data="<p>Write here ...</p>"
+        config={{
+          toolbar: {
+            items: [ 'undo', 'redo',
+            '|', 'heading',
+            '|', 'fontfamily', 'fontsize', 'fontColor', 'fontBackgroundColor',
+            '|', 'bold', 'italic', 'strikethrough', 'subscript', 'superscript', 'code',
+            '|', 'link', 'blockQuote', 'codeBlock',
+            '|', 'bulletedList', 'numberedList', 'todoList', 'outdent', 'indent']
+          }
+        }}        
         onReady={ ( editor ) => {
           console.log( "CKEditor5 React Component is ready to use!", editor );
         } }
         onChange={ ( event, editor ) => {
           const data = editor.getData();
-          console.log( { event, editor, data } );
+          console.log( { data} );
         } }
       />
     </div>
