27e0dfd3a2c4aeb7c8ad7e447c455153a9d41304
unknown <jeetshahajwa@gmail.com>
2024-05-31 00:15:18 +0530

    Mouse Event

--- app/blogs/new/page.tsx
+++ app/blogs/new/page.tsx
@@ -1,13 +1,17 @@
 "use client";
-
-import { useState } from "react";
+import io from 'socket.io-client';
+import { useState, useEffect, useRef } from "react";
 import ElementSection from "../components/elements/element-section";
 import Input from "../components/elements/input";
 import Editor from "../components/rich-text/editor";
-import { FollowerPointerCard } from "@/app/components/motion/following-pointer/following-pointer";
-
+import { FollowerPointerCard, FollowPointer } from "@/app/components/motion/following-pointer/following-pointer";
+const socket = io('http://localhost:3001')
 const NewBlog = () => {
   const [elements, setElements] = useState<string[]>([]);
+  const [coordinates, setCoordinates] = useState({x: 0, y: 0});
+
+  const ref = useRef<HTMLDivElement>(null);
+  const [rect, setRect] = useState<DOMRect | null>(null);
 
   const generateElement = (element: string, index: number) => {
     const mapElements: { [index: string]: React.ReactNode } = {
@@ -23,17 +27,45 @@ const NewBlog = () => {
 
     return <div key={index}>{mapElements[element]}</div>;
   };
+  
+  useEffect(() => {
+    if (ref.current) {
+      setRect(ref.current.getBoundingClientRect());
+    }
+  }, []);
 
   const handleAddElement = (name: string) => {
     const clone = [...elements];
     clone.push(name);
     setElements([...clone]);
   };
+
+  const mouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    console.log("windows", window.scrollX)
+    if(rect)
+    socket.emit('mouse-event', {x: e.clientX - rect.left, y: e.clientY - rect.top})
+  }
   // {!elements.length && (
   //   <p className="text-secondary font-semibold text-2xl text-center">
   //     Add Elements From Section{" "}
   //   </p>
   // )}
+
+  // useEffect(() => {
+  //   // console.log(coordinates);
+  //   // console.log(window.scrollX);
+    
+  // },[coordinates])
+
+  useEffect(() => {
+    socket.on("mouse-receive", (message) => {
+      if(socket.id !== message.id) {
+        console.log("client", message);
+        setCoordinates({x: message.coor.x, y: message.coor.y})
+      }
+    })
+  }, [])
+
   return (
     <div className="flex gap-4 flex-auto px-6 ">
       <div className="w-1/5 flex-auto border-r dark:border-dark-secondary flex flex-col relative">
@@ -45,14 +77,15 @@ const NewBlog = () => {
         </div>
       </div>
       <div className="w-full flex flex-col items-center ">
-        <div className="w-3/5 h-full border">
-          <FollowerPointerCard title="Me">
+        <div className="w-3/5 h-full border relative" ref={ref} onMouseMove={mouseMove}>
+        <FollowPointer x={coordinates.x} y={coordinates.y} />
+          {/* <FollowerPointerCard title="Me">
             {elements.map((element, index) => generateElement(element, index))}
-          </FollowerPointerCard>
+          </FollowerPointerCard> */}
         </div>
       </div>
     </div>
   );
 };
-
+ 
 export default NewBlog;
